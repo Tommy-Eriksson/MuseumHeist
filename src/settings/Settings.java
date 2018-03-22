71ad84423cb40532799217c8bf2e42e035eed906
Tommy <me@tommyeriksson.nu>
2018-03-22 18:27:50 +0100

    added offset and tilesize

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -4,11 +4,23 @@ public class Settings {
 
 	private static final int WIDTH = 1024;
 	private static final int HEIGHT = 768;
+	private static final int OFFSET_X = 212;
+	private static final int OFFSET_Y = 144;
+	private static final int TILE_SIZE = 24;
 	
 	public int getWidth() {
 		return WIDTH;
 	}
 	public int getHeight() {
 		return HEIGHT;
+	}
+	public int getOffsetX() {
+		return OFFSET_X;
+	}
+	public int getOffsetY() {
+		return OFFSET_Y;
+	}
+	public int getTileSize() {
+		return TILE_SIZE;
 	}
 }
