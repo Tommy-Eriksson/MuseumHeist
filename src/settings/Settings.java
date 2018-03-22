c49e4ea3c43aafc4b124f0b83b94999f4f882560
Tommy <me@tommyeriksson.nu>
2018-03-22 18:24:25 +0100

    added class settings

---
+++ src/settings/Settings.java
@@ -0,0 +1,14 @@
+package settings;
+
+public class Settings {
+
+	private static final int WIDTH = 1024;
+	private static final int HEIGHT = 768;
+	
+	public int getWidth() {
+		return WIDTH;
+	}
+	public int getHeight() {
+		return HEIGHT;
+	}
+}
