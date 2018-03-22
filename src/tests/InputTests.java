8fdf7f949b44a305e55166d3d37dda7dc808a9ea
Malin Albinsson <mea733@telia.com>
2018-03-22 14:52:02 +0100

    InputTest first draft. Test of addKey(). Outline of other tests added.

--- src/tests/InputTests.java
+++ src/tests/InputTests.java
@@ -2,13 +2,63 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class InputTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void addKey_shouldAddKeyCodeToArrayListKeys_keyCodeStringAddedLastInArrayListKeys() {
+	// Arrange
+	Input input = new Input();
+	
+	ArrayList<String> expectedArray = new ArrayList<String>();
+	String keyCode="+";
+	expectedArray.add(keyCode);
+	
+	// Act
+	ArrayList<String> resultArray = input.keys; 
+	resultArray = input.addKey(keyCode);
+	
+	// Assert
+			assertEquals(expectedArray, resultArray);
 	}
-
+	
+	@Test 
+	// TODO How should ArrayList keys work? Can the same keycode be found in more than one position in the list?
+	void removeKey_shouldRemoveKeyCodeFromArrayListKeys_keyCodeStringRemovedFromArrayListKeys() {
+	// Arrange
+		fail("Not yet implemented");
+	// Arrange
+	
+	// Act
+	
+	// Assert
+	 
+	}
+	
+	@Test
+	void getKeys_shouldGetKeyCodeFromArrayListKeys_keyCodesFromArrayListKeysReturned() {
+		fail("Not yet implemented");
+	// Arrange
+	
+	// Act
+	
+	// Assert
+	 
+	}
+		
+	/*
+	@Test
+	void metodnamn_informationomtest_förväntatresultat() {
+		fail("Not yet implemented");
+	// Arrange
+	
+	// Act
+	
+	// Assert
+	 
+	}
+*/
 }
