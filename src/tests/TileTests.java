c97a8e3416eb9fb389476738fdf8ae4994edae58
Billy Andersson <anderssonbilly@hotmail.com>
2018-03-24 18:12:55 +0100

    Added setY to entity and added tests for Tile class

--- src/tests/TileTests.java
+++ src/tests/TileTests.java
@@ -1,14 +1,36 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+
+import javax.naming.SizeLimitExceededException;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import asset.Entity;
 
 class TileTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getSize_createTile_expect24() {
+		Tile e = new Entity("assets/wall.png",24);
+		int x = e.getSize();
+		assertEquals(x, 24);
 	}
 
+	@Test
+	void getX_createTileWithNegativ_expectException() {
+		assertThrows(SizeLimitExceededException.class, () -> Tile e = new Entity("assets/wall.png",-5));
+	}
+	
+	@Test
+	@DisplayName("Test that the sprite string is set on creation")
+	void getSprite_createTile_expectSpriteString(){
+		Tile e = new Entity(10, 90, "assets/wall.png");
+		String sprite = e.getSprite();
+		assertEquals(sprite, "assets/wall.png");
+	}
 }
