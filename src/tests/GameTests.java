244648903a272fe2ab57df8223499f8f5ce19747
Billy Andersson <anderssonbilly@hotmail.com>
2018-03-24 15:14:40 +0100

    Added Game Tests, will start implementation

--- src/tests/GameTests.java
+++ src/tests/GameTests.java
@@ -2,13 +2,54 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
+
+import javafx.scene.layout.Pane;
+import logic.Game;
 
 class GameTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getPane_ExpectPane() {
+		Game game = new Game();
+		Pane root = new Pane();
+		assertEqual(root, game.init());
 	}
 
+	@Test
+	void getLevel_ExpectObjectArray() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Game game = new Game();
+		Class<?> gameClass = game.getClass();
+		
+	    Field level = gameClass.getDeclaredField("level");
+		level.setAccessible(true);
+	    
+		Object[][] gameLevel = (Object[][]) level.get(game);
+	    assertEquals(25,gameLevel.length);
+	}
+	
+	@Test
+	void constructorTest() {
+		Game game = new Game();
+		
+		assertNotNull(game);
+	}
+	
+	@Test
+	void startGameLoop_ExpectAnimationTimerToStart() {
+		Game game = new Game();
+		game.start();
+		assertTrue(game.getGameLoopStatus());
+	}
+	
+	@Test
+	void stopGameLoop_ExpectAnimationTimerToStop() {
+		Game game = new Game();
+		game.start();
+		game.stop();
+		assertFalse(game.getGameLoopStatus());
+	}
 }
