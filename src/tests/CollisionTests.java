0df8bc2f42f4cdad62c62c20148f7555a30ec625
Billy Andersson <anderssonbilly@hotmail.com>
2018-03-27 17:00:55 +0200

    Added collision tests

--- src/tests/CollisionTests.java
+++ src/tests/CollisionTests.java
@@ -4,11 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import handler.InputHandler;
+import logic.Game;
+
 class CollisionTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void collisionRightWall_expectTrue() throws Exception {
+		new MockJavaFx().start();
+		InputHandler input = new InputHandler();
+		Game game = new Game();
+		game.init("lasertest",input);
+		Collision collision = new Collision();
+		
+		assertTrue(collision.collide(1,0,"right"));
 	}
 
+	@Test
+	void collisionRightEmptySpace_expectFalse() throws Exception {
+		new MockJavaFx().start();
+		InputHandler input = new InputHandler();
+		Game game = new Game();
+		game.init("lasertest",input);
+		Collision collision = new Collision();
+		
+		assertFalse(collision.collide(2,0,"right"));
+	}
+	
+	@Test
+	void collisionLeftOutsideLevel_expectTrue() throws Exception {
+		new MockJavaFx().start();
+		InputHandler input = new InputHandler();
+		Game game = new Game();
+		game.init("lasertest",input);
+		Collision collision = new Collision();
+		
+		assertTrue(collision.collide(-1,0,"left"));
+	}
 }
